5803eb51fb0435cb73a3a6a693459935dfd39ff1
Neel <neelo2003@gmail.com>
2024-05-30 19:57:34 +0530

    first commit

---
+++ src/Components/GameCircle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Game.css';
+import GameBoard from './GameBoard';
+
+const GameCircle = ({ id, children, className, onCircleClicked }) => {
+    return (
+        <div className={`gameCircle ${className}`} onClick={() => onCircleClicked(id)}>
+            {children}
+        </div>
+    )
+}
+
+export default GameCircle;
+
+// on line 11 (ev) => onClick(ev, id, color)-> without arrow function, it will execute onclick immediately 
